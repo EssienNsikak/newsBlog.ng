31ca6bab4f06fd5e558c06db7a8d34c02f4300e0
EssienNsikak <nsikak.essienceo@gmail.com>
2021-06-11 15:24:47 +0100

    login endpoint added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,4 +22,22 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json('Wrong Username!')
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json('Wrong Password!')
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
