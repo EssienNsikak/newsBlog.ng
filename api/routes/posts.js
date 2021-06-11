9484711da0593b61c5c9a5b3801954789244a3b2
EssienNsikak <nsikak.essienceo@gmail.com>
2021-06-11 22:52:56 +0100

    category and file upload endpoints added

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -15,8 +15,19 @@ router.post('/', async (req, res) => {
 
 // List Posts
 router.get('/all-posts', async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
     try {
-        const posts = await Post.find({});
+        let posts;
+        if (username) {
+            posts = await Post.find({ username });
+        } else if  (catName) {
+            posts = await Post.find({ categories: {
+                $in: [catName]
+            }});
+        } else {
+            posts = await Post.find();
+        }
         res.status(200).json(posts)            
     } catch (err) {
         res.status(500).json(err);
